504efe8a17dc317a644525e1b7796ca023a254e9
ALNoman78 <abdullaalno078@gmail.com>
2024-09-09 00:50:04 +0600

    bug fixed

--- password generator.js
+++ password generator.js
@@ -1,11 +1,13 @@
 const obj = {
     name : "kolimuddin",
-    birthYear : 1999,
+    birthYear : 1990,
     siteName : "google",
 }
 
 function passwordGen(pass){
-    if(pass.name === undefined || pass.birthYear === undefined || pass.siteName === undefined || pass.birthYear !== 4);
+    if(pass.name === undefined || pass.birthYear === undefined || pass.siteName === undefined || pass.birthYear.toString().length !== 4){
+        return "Error"
+    };
     let site = pass.siteName.charAt(0).toUpperCase()+ pass.siteName.slice(1);
     let newPasswordGenerate = site + '#' + pass.name + '@' + pass.birthYear
     return newPasswordGenerate;
