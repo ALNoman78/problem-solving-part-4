ccba20ffdbe20fe6b6d1f256cacbd91f93af405a
ALNoman78 <abdullaalno078@gmail.com>
2024-09-10 10:44:30 +0600

    last charecter looping

---
+++ last char.js
@@ -0,0 +1,16 @@
+const userName = "moule"
+
+function checkName(name) {
+    let char = "a,e,i,o,u,w,x";
+    let newChar = char.split(",")
+    let result = "Bad Name";
+    let check = name[name.length - 1]
+    for(let i = 0; i < newChar.length ; i++){
+        if (newChar[i] === check) {
+            result = "Good Name";
+        }
+    }
+    return result;
+}
+const value = checkName(userName);
+console.log(value);
